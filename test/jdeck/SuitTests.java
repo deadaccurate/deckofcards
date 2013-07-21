79b15ea6a4a785794b3537bd78f252f69a0e64e4
Jon <jesusfreak4o@netscape.net>
2013-07-20 23:06:29 -0400

    implemented shuffle and added comments

---
+++ test/jdeck/SuitTests.java
@@ -0,0 +1,16 @@
+package jdeck;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SuitTests
+{
+
+    @Test
+    public void test()
+    {
+        fail("Not yet implemented");
+    }
+
+}
