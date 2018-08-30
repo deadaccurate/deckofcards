174683979b7f1da9e573ee9ecdc66d2dc9e47708
Jonathan Buttner <jonathan.e.buttner@gmail.com>
2018-08-29 20:56:48 -0400

    project structure

---
+++ src/test/java/com/deadaccurate/DoTest.java
@@ -0,0 +1,13 @@
+package com.deadaccurate;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DoTest {
+    @Test
+    public void testSomething() throws Exception {
+        Assert.assertFalse(true);
+    }
+
+}
